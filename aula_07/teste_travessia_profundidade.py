ed63586ff14078e7981caf8ca01e9adfea042774
Renzo <renzon@gmail.com>
2021-06-16 21:28:43 -0300

    Busca em profundidade resolvida
    Aula https://youtu.be/o1bkZuDXIlk

--- aula_07/teste_travessia_profundidade.py
+++ aula_07/teste_travessia_profundidade.py
@@ -1,12 +1,128 @@
-class Noh:
-    pass
+from collections import deque
+from unittest.case import TestCase
 
+
+class Noh:
+    def __init__(self, valor, pai: 'Noh' = None):
+        self.valor = valor
+        self.pai = None
+        if pai:
+            pai.adicionar(self)
+        self.irmao_direito = None
+        self.filho_esquerdo = None
+
+    def adicionar(self, filho):
+        if self.filho_esquerdo is None:
+            self.filho_esquerdo = filho
+        else:
+            filho_sem_irmao_direito = self.filho_esquerdo
+            while filho_sem_irmao_direito.irmao_direito is not None:
+                filho_sem_irmao_direito = filho_sem_irmao_direito.irmao_direito
+            filho_sem_irmao_direito.irmao_direito = filho
+        filho.pai = self
+    def __repr__(self):
+        return repr(self.valor)
 
 class Arvore:
-    pass
+    def __init__(self, raiz: Noh = None):
+        self.raiz = raiz
+
+    def altura(self):
+        altura_maxima = 0
+        altura_atual = 0
+        pilha = deque()
+        pilha.append((self.raiz, altura_atual))
+        while pilha:
+            noh_atual, altura_atual = pilha.pop()
+            if noh_atual is None:
+                continue
+            pilha.append((noh_atual.irmao_direito, altura_atual))
+            altura_atual += 1
+            altura_maxima = max(altura_maxima, altura_atual)
+            pilha.append((noh_atual.filho_esquerdo, altura_atual))
+        return altura_maxima
+
+    def __iter__(self):
+        # pre ordem
+        # pilha = deque()
+        # pilha.append(self.raiz)
+        # while pilha:
+        #     noh_atual = pilha.pop()
+        #     if noh_atual is None:
+        #         continue
+        #     yield noh_atual.valor
+        #     pilha.append(noh_atual.irmao_direito)
+        #     pilha.append(noh_atual.filho_esquerdo)
 
 
-from unittest.case import TestCase
+        pilha = deque()
+        pilha.append((self.raiz, True))
+        while pilha:
+            noh_atual, procurar_filhos = pilha.pop()
+            if noh_atual is None:
+                continue
+            if procurar_filhos:
+                pilha.append((noh_atual.irmao_direito, True))
+                pilha.append((noh_atual, False))
+                pilha.append((noh_atual.filho_esquerdo, True))
+            else:
+                yield noh_atual.valor
+
+
+class ArvoreTestes(TestCase):
+    def teste_init(self):
+        arvore = Arvore()
+        self.assertIsNone(arvore.raiz)
+
+    def teste_arvore_com_raiz(self):
+        noh = Noh(1)
+        arvore = Arvore(noh)
+        self.assertIs(noh, arvore.raiz)
+
+    def teste_altura_arvore(self):
+        self.assertEqual(0, Arvore().altura())
+        self.assertEqual(1, Arvore(Noh(1)).altura())
+        arvore_binaria = self.gerar_arvore_binaria()
+        self.assertEqual(4, arvore_binaria.altura())
+
+    def test_travesia_em_profundidade(self):
+        travessia_arvore_vazia = [i for i in Arvore()]
+        self.assertListEqual([], travessia_arvore_vazia)
+        travessia_arvore_unitaria = [i for i in Arvore(Noh(1))]
+        self.assertListEqual([1], travessia_arvore_unitaria)
+        arvore_binaria = self.gerar_arvore_binaria()
+        travessia_arvore_binaria = [i for i in arvore_binaria]
+        pos_ordem = [0, 1, 3, 4, 2, 6, 7, 9, 8, 5]
+        pre_ordem = [5, 2, 1, 0, 4, 3, 8, 7, 6, 9]
+        self.assertTrue(travessia_arvore_binaria == pos_ordem or travessia_arvore_binaria == pre_ordem)
+
+    def gerar_arvore_binaria(self):
+        """
+                                    5
+                      2                       8
+                1         4             7          9
+            0                 3     6
+        
+        :return:
+        """
+        nohs = [Noh(i) for i in range(10)]
+        raiz = nohs[5]
+        raiz.adicionar(nohs[2])
+        raiz.adicionar(nohs[8])
+
+        nohs[2].adicionar(nohs[1])
+        nohs[2].adicionar(nohs[4])
+
+        nohs[1].adicionar(nohs[0])
+
+        nohs[4].adicionar(nohs[3])
+
+        nohs[8].adicionar(nohs[7])
+        nohs[8].adicionar(nohs[9])
+
+        nohs[7].adicionar(nohs[6])
+
+        return Arvore(raiz)
 
 
 class NohTestes(TestCase):
@@ -61,51 +177,3 @@ class NohTestes(TestCase):
         self.assertIs(filho2, filho.irmao_direito)
         self.assertIs(filho3, filho2.irmao_direito)
         self.assertIsNone(filho3.irmao_direito)
-
-
-class ArvoreTestes(TestCase):
-    def teste_init(self):
-        arvore = Arvore()
-        self.assertIsNone(arvore.raiz)
-
-    def teste_arvore_com_raiz(self):
-        noh = Noh(1)
-        arvore = Arvore(noh)
-        self.assertIs(noh, arvore.raiz)
-
-    def teste_altura_arvore(self):
-        self.assertEqual(0, Arvore().altura())
-        self.assertEqual(1, Arvore(Noh(1)).altura())
-        arvore_binaria = self.gerar_arvore_binaria()
-        self.assertEqual(4, arvore_binaria.altura())
-
-    def test_travesia_em_profundidade(self):
-        travessia_arvore_vazia = [i for i in Arvore()]
-        self.assertListEqual([], travessia_arvore_vazia)
-        travessia_arvore_unitaria = [i for i in Arvore(Noh(1))]
-        self.assertListEqual([1], travessia_arvore_unitaria)
-        arvore_binaria = self.gerar_arvore_binaria()
-        travessia_arvore_binaria = [i for i in arvore_binaria]
-        pos_ordem = [0, 1, 3, 4, 2, 6, 7, 9, 8, 5]
-        pre_ordem = [5, 2, 1, 0, 4, 3, 8, 7, 6, 9]
-        self.assertTrue(travessia_arvore_binaria == pos_ordem or travessia_arvore_binaria == pre_ordem)
-
-    def gerar_arvore_binaria(self):
-        nohs = [Noh(i) for i in range(10)]
-        raiz = nohs[5]
-        raiz.adicionar(nohs[2])
-        raiz.adicionar(nohs[8])
-
-        nohs[2].adicionar(nohs[1])
-        nohs[2].adicionar(nohs[4])
-
-        nohs[1].adicionar(nohs[0])
-
-        nohs[4].adicionar(nohs[3])
-
-        nohs[8].adicionar(nohs[7])
-        nohs[8].adicionar(nohs[9])
-
-        nohs[7].adicionar(nohs[6])
-
-        return Arvore(raiz)
